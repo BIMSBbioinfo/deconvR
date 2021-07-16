8ec70cba7267974143f84b6fea43aedee9427e92
igunduz <bgunduz0012@gmail.com>
2021-07-16 13:05:07 +0300

    Minor Updates
    Minor changes were made in the vignette structure

--- R/deconvolute.R
+++ R/deconvolute.R
@@ -7,12 +7,12 @@
 #' @param reference A dataframe containing signatures of different cell types
 #' (e.g. methylation signature) used to train the model. The first column
 #' should contain a unique ID (e.g. target ID) to match rows of the reference
-#' to rows of the bulk. All subsequent columns are cell types.
-#'  One row per unit of the signature (e.g. CpG). Each cell contains the value
-#'   for the cell type for this unit (e.g. methylation value of the CpG).
-#'  If not given, defaults to a reference atlas which is included in this
-#'  package (see deconvR/inst/reference_atlas_nodup.RDS).
-#'  This reference atlas comes from Moss et al. (2018)
+#' to rows of the bulk. All subsequent columns are cell types. One row per unit
+#' of the signature (e.g. CpG). Each cell contains the value for the cell type
+#' of this unit (e.g. methylation value of the CpG). If not given, defaults to
+#' a reference atlas which is included in this package.
+#' (see deconvR/inst/reference_atlas_nodup.RDS).
+#' This reference atlas comes from Moss et al. (2018)
 #' @param vec The user may provide a vector with which partial R-squared of the
 #' results will be calculated.
 #' The length must match the number of rows of the reference and bulk tables
@@ -21,7 +21,7 @@
 #' @param bulk A dataframe containing signatures of bulk samples used to test
 #' to model.
 #' Should be dataframe with first column with unique IDs (does not need to
-#' xactly match list of IDs in reference,but should have significant overlap),
+#' exactly match list of IDs in reference,but should have significant overlap),
 #' and rest of columns = samples. Should not have duplicate IDs. May use
 #' simulateCellMix function to create this dataframe.
 #' @param model A string indicating which model is used to deconvolute the
@@ -34,15 +34,14 @@
 #' deconvolute(
 #'   reference = readRDS(system.file("reference_atlas_nodup.RDS",
 #'   package = "deconvR")),
-#'   bulk = simulateCellMix(5)[[1]], model = "rlm"
-#' )
+#'   bulk = simulateCellMix(5)[[1]], model = "rlm" )
 #' @return A list, first is a dataframe which contains predicted cell-type
 #' proportions of bulk methylation profiles in "bulk", second is a list of
-#' partial-rsq values of results, one value per sample
+#' partial-rsq values of results, one value per sample.
 #' @references Moss, J. et al.  (2018). Comprehensive human cell-type
-#'  methylation atlas reveals origins of circulating cell-free DNA in health
-#'  and disease. Nature communications, 9(1), 1-12.
-#'  \url{https://doi.org/10.1038/s41467-018-07466-6}
+#' methylation atlas reveals origins of circulating cell-free DNA in health
+#' and disease. Nature communications, 9(1), 1-12.
+#' \url{https://doi.org/10.1038/s41467-018-07466-6}
 #' @export
 
 deconvolute <- function(reference =
@@ -85,7 +84,8 @@ deconvolute <- function(reference =
     return(rsq_partial)
   }
 
-  if (model == "nnls") { # non negative least squares
+  if (model == "nnls") {
+    # non negative least squares
     oper <- foreach::foreach(h = seq(2, ncol(bulk)), .inorder = TRUE,
                              .combine = "comb", .multicombine = TRUE,
                              .init = list(c(), list())) %dopar% {
--- R/simulateCellMix.R
+++ R/simulateCellMix.R
@@ -4,18 +4,17 @@
 #' the dataframe.
 #' @param mixingVector Specify the cell origin proportions.
 #' If numberOfSamples = 1, this can be a vector of length = number of cell
-#' types in reference.
-#' Otherwise, this is a dataframe with rows for cell types (must be equal to
-#' cell types in reference) and columns for samples.
-#' Cells contain the proportion of the sample from the cell type. Use zeros for
-#' any unused cell type. If this object is not given,will use random values for
-#' the simulation.
+#' types in reference. Otherwise, this is a dataframe with rows for cell types
+#' (must be equal to cell types in reference) and columns for samples. Cells
+#' contain the proportion of the sample from the cell type. Use zeros for any
+#' unused cell type. If this object is not given,will use random values for the
+#' simulation.
 #' @param reference A dataframe containing signatures of different cell types
 #' used to generate the simulation. The first column should contain a unique ID
 #' (e.g. CpG target ID) which can be used in deconvolution to match rows of the
-#'  reference to rows of the bulk. All subsequent columns are cell types.
-#' Rows are units of the signature. Each cell contains the value for the cell
-#' type and signature unit (e.g. methylation value at this CpG). If not given,
+#' reference to rows of the bulk. All subsequent columns are cell types. Rows
+#' are units of the signature. Each cell contains the value for the cell type
+#' and signature unit (e.g. methylation value at this CpG). If not given,
 #' defaults to a reference atlas which is included in this package
 #' (see deconvR/inst/reference_atlas_nodup.RDS).
 #' This reference atlas comes from Moss et al. (2018)
@@ -31,18 +30,17 @@
 #' simulateCellMix(1, mixingVector = c(0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 #' 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
 #' simulateCellMix(1, data.frame(c(0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0,
-#'  0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0)))
+#' 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0)))
 #' simulateCellMix(2, data.frame(
-#'   c(0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0,
+#' c(0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0,
 #'    0, 0),
-#'   c(0.1, 0, 0, 0, 0, 0, 0.5, 0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0.2, 0, 0, 0, 0, 0,
-#'    0, 0, 0)
-#' ))
+#' c(0.1, 0, 0, 0, 0, 0, 0.5, 0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0.2, 0, 0, 0, 0, 0,
+#'    0, 0, 0) ))
 #' @return A list containing two data frames.
 #' First: A dataframe which contains mixed cell-type origin simulated samples.
 #' The first column contains a unique ID (used from reference) which can be
 #' used in deconvolution to match rows of the reference to rows of the bulk.All
-#' subsequent columns are cell types. Rows are units of signature (e.g. CpGs)
+#' subsequent columns are cell types. Rows are units of signature (e.g. CpGs).
 #' Each cell contains the value for the cell type and unit (e.g. methylation
 #' value at this CpG) Second: A dataframe with the cell proportions of the
 #' generated samples. Each row is a sample. Columns are cell types.
@@ -104,8 +102,10 @@ simulateCellMix <- function(numberOfSamples, mixingVector = NULL,
         stop("mixingVector should only contain numbers")
       }
       else {
-        for (s in seq_len(ncol(mixingVector))) { # each row is a sample
-          for (t in seq_len(nrow(mixingVector))) { # each column is a cell type
+        for (s in seq_len(ncol(mixingVector))) {
+          # each row is a sample
+          for (t in seq_len(nrow(mixingVector))) {
+            # each column is a cell type
             if (mixingVector[t, s] > 0) {
               simulatedMixtureTable[, s + 1] <- (simulatedMixtureTable[, s + 1]
               +(mixingVector[t, s] * reference[, t + 1]))
@@ -121,7 +121,8 @@ simulateCellMix <- function(numberOfSamples, mixingVector = NULL,
     else if (class(mixingVector) == "numeric") {
       if (1 != numberOfSamples) {
         stop("you may only use a vector for mixingVector if numberOfSamples = 1,
-      otherwise use dataframe with columns for samples and rows for cell type")
+             otherwise use dataframe with columns for samples and rows for cell
+             type")
       }
       if (typeof(mixingVector) != "double") {
         stop("mixingVector should only contain numbers")
