27c2474d096f5981eae07b9ba5a2c8c1cf25fc34
İrem Gündüz <bgunduz0012@gmail.com>
2021-09-25 22:59:31 +0300

    update Rds to fix warning

--- R/data.R
+++ R/data.R
@@ -1,12 +1,11 @@
 #' A dataset Illumina probe IDs of 500,000 genomic loci (identified using the
 #' “seqnames”, “ranges”, and “strand” values).
-#'
+#' @usage data(probe_ids)
 #' @format GRanges object with 500000 ranges and 1 metadata column:
 #' \describe{
 #'   \item{metadata}{contains seqnames ranges strand and ID}
 #'   ...
 #' }
-#' @usage data(probe_ids)
 #' @source \url{https://support.illumina.com/downloads/infinium-methylationepic-v1-0-product-files.html}
 "probe_ids"
 
@@ -14,12 +13,12 @@
 #' This object contains the methylation values of 11715 genomic loci
 #' (identified using the “seqnames”, “ranges”, and “strand” values) for a single
 #'  example sample. It can be used directly as WGBS_data in BSmeth2Probe.
+#'  @usage data(WGBS_GRanges)
 #' @format GRanges object with 11715 ranges and 1 metadata column:
 #' \describe{
 #'   \item{metadata}{contains seqnames ranges strand and ID.
 #'   seqinfo: 25 sequences from an unspecified genome; no seqlengths}
 #'   ...
 #' }
-#' @usage data(WGBS_GRanges)
 #' @source \url{https://www.encodeproject.org/data-standards/wgbs/}
 "WGBS_GRanges"
