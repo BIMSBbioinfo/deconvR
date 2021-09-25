81d9658e42a69f74c68582c5254272e6e497dc58
İrem Gündüz <bgunduz0012@gmail.com>
2021-09-25 22:45:55 +0300

    update data R files

--- R/data.R
+++ R/data.R
@@ -1,3 +1,14 @@
+#' A dataset Illumina probe IDs of 500,000 genomic loci (identified using the
+#' “seqnames”, “ranges”, and “strand” values).
+#'
+#' @format GRanges object with 500000 ranges and 1 metadata column:
+#' \describe{
+#'   \item{metadata}{contains seqnames ranges strand and ID}
+#'   ...
+#' }
+#' @source \url{https://support.illumina.com/downloads/infinium-methylationepic-v1-0-product-files.html}
+"probe_ids"
+
 #' An example of WGBS data has been included as the GRanges object
 #' This object contains the methylation values of 11715 genomic loci
 #' (identified using the “seqnames”, “ranges”, and “strand” values) for a single
