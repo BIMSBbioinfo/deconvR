924f42f784f18a231d4c5ad49612b07ca4b9a550
İrem Gündüz <irembgunduz@gmail.com>
2021-09-29 20:44:00 +0300

    update styling

--- tests/testthat/test_BSmeth2Probe.R
+++ tests/testthat/test_BSmeth2Probe.R
@@ -5,11 +5,18 @@ test_that("BSmeth2Probe", {
     data("probe_ids")
     probe_ids_df <- GenomicRanges::as.data.frame(probe_ids)
 
-    results <- BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = WGBS_GRanges)
+    results <- BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = WGBS_GRanges
+    )
 
     expect_equal(class(results), "data.frame")
-    expect_equal(NCOL(results), 1 + length(colnames(GenomicRanges::mcols(WGBS_GRanges))))
-    expect_equal(colnames(results), c("IDs", colnames(GenomicRanges::mcols(WGBS_GRanges))))
+    expect_equal(NCOL(results), 1 +
+        length(colnames(GenomicRanges::mcols(WGBS_GRanges))))
+    expect_equal(colnames(results), c(
+        "IDs",
+        colnames(GenomicRanges::mcols(WGBS_GRanges))
+    ))
     expect_equal(class(results[[2]]), "numeric")
     expect_equal(class(results[[1]]), "character")
     expect_gt(NROW(results), 0)
@@ -19,56 +26,123 @@ test_that("BSmeth2Probe", {
     expect_lt(NROW(results), NROW(probe_ids))
     expect_lt(NROW(results), NROW(WGBS_GRanges))
 
-    expect_error(NROW(BSmeth2Probe(probe_id_locations = probe_ids[0], WGBS_data = WGBS_GRanges)))
-    expect_error(NROW(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = WGBS_GRanges[0])))
+    expect_error(NROW(BSmeth2Probe(
+        probe_id_locations = probe_ids[0],
+        WGBS_data = WGBS_GRanges
+    )))
+    expect_error(NROW(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = WGBS_GRanges[0]
+    )))
 
-    expect_lt(NROW(results), NROW(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = WGBS_GRanges, cutoff = 100)))
-    expect_gt(NROW(results), NROW(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = WGBS_GRanges, cutoff = 0)))
-    expect_lt(NROW(results), NROW(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = WGBS_GRanges, multipleMapping = TRUE)))
-    expect_gt(NROW(results), NROW(BSmeth2Probe(probe_id_locations = probe_ids[1:NROW(probe_ids) / 2], WGBS_data = WGBS_GRanges)))
-    expect_gt(NROW(results), NROW(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = WGBS_GRanges[1:NROW(WGBS_GRanges) / 2])))
+    expect_lt(NROW(results), NROW(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = WGBS_GRanges,
+        cutoff = 100
+    )))
+    expect_gt(NROW(results), NROW(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = WGBS_GRanges,
+        cutoff = 0
+    )))
+    expect_lt(NROW(results), NROW(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = WGBS_GRanges,
+        multipleMapping = TRUE
+    )))
+    expect_gt(NROW(results), NROW(BSmeth2Probe(
+        probe_id_locations =
+            probe_ids[1:NROW(probe_ids) / 2],
+        WGBS_data = WGBS_GRanges
+    )))
+    expect_gt(NROW(results), NROW(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data =
+            WGBS_GRanges[1:NROW(WGBS_GRanges) / 2]
+    )))
 
-    expect_equal(results, BSmeth2Probe(probe_id_locations = probe_ids_df, WGBS_data = WGBS_GRanges))
+    expect_equal(results, BSmeth2Probe(
+        probe_id_locations = probe_ids_df,
+        WGBS_data = WGBS_GRanges
+    ))
     expect_equal(
-        colnames(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = methylKit::methRead(list(
-            system.file("extdata", "test1.myCpG.txt", package = "methylKit"),
-            system.file("extdata", "test2.myCpG.txt", package = "methylKit")
-        ),
-        sample.id = list("test1", "test2"), assembly = "hg18", treatment = c(1, 1), context = "CpG"
-        ))),
+        colnames(BSmeth2Probe(
+            probe_id_locations = probe_ids,
+            WGBS_data = methylKit::methRead(list(
+                system.file("extdata", "test1.myCpG.txt", package = "methylKit"),
+                system.file("extdata", "test2.myCpG.txt", package = "methylKit")
+            ),
+            sample.id = list("test1", "test2"), assembly = "hg18",
+            treatment = c(1, 1), context = "CpG"
+            )
+        )),
         c("IDs", methylKit::getSampleID(methylKit::methRead(list(
             system.file("extdata", "test1.myCpG.txt", package = "methylKit"),
             system.file("extdata", "test2.myCpG.txt", package = "methylKit")
         ),
-        sample.id = list("test1", "test2"), assembly = "hg18", treatment = c(1, 1), context = "CpG"
+        sample.id = list("test1", "test2"), assembly = "hg18",
+        treatment = c(1, 1), context = "CpG"
         )))
     )
     expect_equal(
-        colnames(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = methylKit::methRead(list(
-            system.file("extdata", "test1.myCpG.txt", package = "methylKit"),
-            system.file("extdata", "test2.myCpG.txt", package = "methylKit")
-        ),
-        sample.id = list("test1", "test2"), assembly = "hg18", treatment = c(1, 1), context = "CpG"
-        ))),
-        colnames(BSmeth2Probe(cutoff = 100, probe_id_locations = probe_ids, WGBS_data = methylKit::methRead(list(
-            system.file("extdata", "test1.myCpG.txt", package = "methylKit"),
-            system.file("extdata", "test2.myCpG.txt", package = "methylKit")
-        ),
-        sample.id = list("test1", "test2"), assembly = "hg18", treatment = c(1, 1), context = "CpG"
-        )))
+        colnames(BSmeth2Probe(
+            probe_id_locations = probe_ids,
+            WGBS_data = methylKit::methRead(list(
+                system.file("extdata", "test1.myCpG.txt", package = "methylKit"),
+                system.file("extdata", "test2.myCpG.txt", package = "methylKit")
+            ),
+            sample.id = list("test1", "test2"), assembly = "hg18",
+            treatment = c(1, 1), context = "CpG"
+            )
+        )),
+        colnames(BSmeth2Probe(
+            cutoff = 100, probe_id_locations = probe_ids,
+            WGBS_data = methylKit::methRead(list(
+                system.file("extdata", "test1.myCpG.txt", package = "methylKit"),
+                system.file("extdata", "test2.myCpG.txt", package = "methylKit")
+            ),
+            sample.id = list("test1", "test2"), assembly = "hg18",
+            treatment = c(1, 1), context = "CpG"
+            )
+        ))
     )
 
-    simulatedData <- methylKit::dataSim(replicates = 4, sites = 200000, treatment = c(1, 1, 0, 0), percentage = 10, effect = 25)
-    expect_gte(NROW(BSmeth2Probe(probe_id_locations = probe_ids_df, WGBS_data = simulatedData)), 0)
+    simulatedData <- methylKit::dataSim(
+        replicates = 4, sites = 200000,
+        treatment = c(1, 1, 0, 0),
+        percentage = 10, effect = 25
+    )
+    expect_gte(NROW(BSmeth2Probe(
+        probe_id_locations = probe_ids_df,
+        WGBS_data = simulatedData
+    )), 0)
     expect_equal(
-        NCOL(BSmeth2Probe(probe_id_locations = probe_ids_df, WGBS_data = simulatedData)),
+        NCOL(BSmeth2Probe(
+            probe_id_locations = probe_ids_df,
+            WGBS_data = simulatedData
+        )),
         length(methylKit::getSampleID(simulatedData)) + 1
     )
 
 
-    expect_error(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = WGBS_GRanges, cutoff = -1))
-    expect_error(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = probe_ids))
-    expect_error(BSmeth2Probe(probe_id_locations = WGBS_GRanges, WGBS_data = WGBS_GRanges))
-    expect_error(BSmeth2Probe(probe_id_locations = NULL, WGBS_data = WGBS_GRanges))
-    expect_error(BSmeth2Probe(probe_id_locations = probe_ids, WGBS_data = NULL))
+    expect_error(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = WGBS_GRanges, cutoff = -1
+    ))
+    expect_error(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = probe_ids
+    ))
+    expect_error(BSmeth2Probe(
+        probe_id_locations = WGBS_GRanges,
+        WGBS_data = WGBS_GRanges
+    ))
+    expect_error(BSmeth2Probe(
+        probe_id_locations = NULL,
+        WGBS_data = WGBS_GRanges
+    ))
+    expect_error(BSmeth2Probe(
+        probe_id_locations = probe_ids,
+        WGBS_data = NULL
+    ))
 })
--- tests/testthat/test_deconvolute.R
+++ tests/testthat/test_deconvolute.R
@@ -1,7 +1,9 @@
 library(deconvR)
 
 test_that("deconvolute nnls", {
-    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS", package = "deconvR"))
+    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS",
+        package = "deconvR"
+    ))
     simulation <- simulateCellMix(5)
     bulkTable <- simulation[[1]]
     proportionsTable <- simulation[[2]]
@@ -10,15 +12,29 @@ test_that("deconvolute nnls", {
     results <- deconvolution[[1]]
     partial_rsq <- deconvolution[[2]]
 
-    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas, by = "IDs"))[, -1])
+    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas,
+        by = "IDs"
+    ))[, -1])
 
-    expect_output(deconvolute(bulk = bulkTable), message("DECONVOLUTION WITH NNLS"))
-    expect_output(deconvolute(bulk = bulkTable), message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR NNLS : "))
+    expect_output(
+        deconvolute(bulk = bulkTable),
+        message("DECONVOLUTION WITH NNLS")
+    )
+    expect_output(
+        deconvolute(bulk = bulkTable),
+        message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR NNLS : ")
+    )
 
     expect_equal(dim(results), c(5, length(colnames(ref_atlas[, -1]))))
-    expect_equal(dim(deconvolute(bulk = bulkTable[-1, ])[[1]]), c(5, length(colnames(ref_atlas[, -1]))))
+    expect_equal(
+        dim(deconvolute(bulk = bulkTable[-1, ])[[1]]),
+        c(5, length(colnames(ref_atlas[, -1])))
+    )
     expect_equal(colnames(results), colnames(ref_atlas[, -1]))
-    expect_equal(lapply(unlist(results[, ]), typeof)[[1]], print("double"), ignore_attr = TRUE)
+    expect_equal(lapply(unlist(results[, ]), typeof)[[1]],
+        print("double"),
+        ignore_attr = TRUE
+    )
     expect_equal(sum(results[1, ]), 1)
     expect_equal(sum(results[2, ]), 1)
     expect_equal(sum(results[3, ]), 1)
@@ -26,8 +42,14 @@ test_that("deconvolute nnls", {
     expect_equal(sum(results[5, ]), 1)
     expect_error(deconvolute())
     expect_error(deconvolute(bulk = bulkTable, model = "n"))
-    expect_error(deconvolute(bulk = bulkTable, model = "nnls", vec = rep_len(1, vectorlength)), NA)
-    expect_error(deconvolute(bulk = bulkTable, model = "nnls", vec = rep_len(1, vectorlength - 1)))
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "nnls",
+        vec = rep_len(1, vectorlength)
+    ), NA)
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "nnls",
+        vec = rep_len(1, vectorlength - 1)
+    ))
     expect_error(deconvolute(bulk = bulkTable[, -1], model = "nnls"))
     expect_error(deconvolute(model = "nnls"))
     expect_equal(dim(results), dim(proportionsTable))
@@ -39,7 +61,9 @@ test_that("deconvolute nnls", {
 })
 
 test_that("deconvolute svr", {
-    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS", package = "deconvR"))
+    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS",
+        package = "deconvR"
+    ))
     simulation <- simulateCellMix(5)
     bulkTable <- simulation[[1]]
     proportionsTable <- simulation[[2]]
@@ -48,26 +72,51 @@ test_that("deconvolute svr", {
     results <- deconvolution[[1]]
     partial_rsq <- deconvolution[[2]]
 
-    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas, by = "IDs"))[, -1])
+    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas,
+        by = "IDs"
+    ))[, -1])
 
-    expect_output(deconvolute(bulk = bulkTable, model = "svr"), message("DECONVOLUTION WITH SVR"))
-    expect_output(deconvolute(bulk = bulkTable, model = "svr"), message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR SVR : "))
+    expect_output(
+        deconvolute(bulk = bulkTable, model = "svr"),
+        message("DECONVOLUTION WITH SVR")
+    )
+    expect_output(
+        deconvolute(bulk = bulkTable, model = "svr"),
+        message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR SVR : ")
+    )
 
-    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS", package = "deconvR"))
+    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS",
+        package = "deconvR"
+    ))
     simulation <- simulateCellMix(5)
     bulkTable <- simulation[[1]]
     proportionsTable <- simulation[[2]]
     expect_equal(dim(results), c(5, length(colnames(ref_atlas[, -1]))))
-    expect_equal(dim(deconvolute(bulk = bulkTable[-1, ], model = "svr")[[1]]), c(5, length(colnames(ref_atlas[, -1]))))
+    expect_equal(
+        dim(deconvolute(
+            bulk = bulkTable[-1, ],
+            model = "svr"
+        )[[1]]),
+        c(5, length(colnames(ref_atlas[, -1])))
+    )
     expect_equal(colnames(results), colnames(ref_atlas[, -1]))
-    expect_equal(lapply(unlist(results[, ]), typeof)[[1]], print("double"), ignore_attr = TRUE)
+    expect_equal(lapply(unlist(results[, ]), typeof)[[1]],
+        print("double"),
+        ignore_attr = TRUE
+    )
     expect_equal(sum(results[2, ]), 1)
     expect_equal(sum(results[3, ]), 1)
     expect_equal(sum(results[4, ]), 1)
     expect_equal(sum(results[5, ]), 1)
     expect_error(deconvolute(bulk = bulkTable[, -1], model = "svr"))
-    expect_error(deconvolute(bulk = bulkTable, model = "svr", vec = rep_len(1, vectorlength)), NA)
-    expect_error(deconvolute(bulk = bulkTable, model = "svr", vec = rep_len(1, vectorlength - 1)))
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "svr",
+        vec = rep_len(1, vectorlength)
+    ), NA)
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "svr",
+        vec = rep_len(1, vectorlength - 1)
+    ))
     expect_error(deconvolute(model = "svr"))
     expect_equal(dim(results), dim(proportionsTable))
     expect_equal(colnames(results), colnames(ref_atlas[, -1]))
@@ -77,7 +126,9 @@ test_that("deconvolute svr", {
 })
 
 test_that("deconvolute qp", {
-    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS", package = "deconvR"))
+    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS",
+        package = "deconvR"
+    ))
     simulation <- simulateCellMix(5)
     bulkTable <- simulation[[1]]
     proportionsTable <- simulation[[2]]
@@ -86,23 +137,42 @@ test_that("deconvolute qp", {
     results <- deconvolution[[1]]
     partial_rsq <- deconvolution[[2]]
 
-    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas, by = "IDs"))[, -1])
+    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas,
+        by = "IDs"
+    ))[, -1])
 
-    expect_output(deconvolute(bulk = bulkTable, model = "qp"), message("DECONVOLUTION WITH QP"))
-    expect_output(deconvolute(bulk = bulkTable, model = "qp"), message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR QP : "))
+    expect_output(
+        deconvolute(bulk = bulkTable, model = "qp"),
+        message("DECONVOLUTION WITH QP")
+    )
+    expect_output(
+        deconvolute(bulk = bulkTable, model = "qp"),
+        message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR QP : ")
+    )
 
     expect_equal(dim(results), c(5, length(colnames(ref_atlas[, -1]))))
-    expect_equal(dim(deconvolute(bulk = bulkTable[-1, ], model = "qp")[[1]]), c(5, length(colnames(ref_atlas[, -1]))))
+    expect_equal(
+        dim(deconvolute(bulk = bulkTable[-1, ], model = "qp")[[1]]),
+        c(5, length(colnames(ref_atlas[, -1])))
+    )
     expect_equal(colnames(results), colnames(ref_atlas[, -1]))
-    expect_equal(lapply(unlist(results[, ]), typeof)[[1]], print("double"), ignore_attr = TRUE)
+    expect_equal(lapply(unlist(results[, ]), typeof)[[1]], print("double"),
+        ignore_attr = TRUE
+    )
     expect_equal(sum(results[1, ]), 1)
     expect_equal(sum(results[2, ]), 1)
     expect_equal(sum(results[3, ]), 1)
     expect_equal(sum(results[4, ]), 1)
     expect_equal(sum(results[5, ]), 1)
     expect_error(deconvolute(bulk = bulkTable[, -1], model = "qp"))
-    expect_error(deconvolute(bulk = bulkTable, model = "qp", vec = rep_len(1, vectorlength)), NA)
-    expect_error(deconvolute(bulk = bulkTable, model = "qp", vec = rep_len(1, vectorlength - 1)))
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "qp",
+        vec = rep_len(1, vectorlength)
+    ), NA)
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "qp",
+        vec = rep_len(1, vectorlength - 1)
+    ))
     expect_error(deconvolute(model = "qp"))
     expect_equal(dim(results), dim(proportionsTable))
     expect_equal(colnames(results), colnames(ref_atlas[, -1]))
@@ -112,7 +182,9 @@ test_that("deconvolute qp", {
 })
 
 test_that("deconvolute rlm", {
-    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS", package = "deconvR"))
+    ref_atlas <- readRDS(system.file("reference_atlas_nodup.RDS",
+        package = "deconvR"
+    ))
     simulation <- simulateCellMix(5)
     bulkTable <- simulation[[1]]
     proportionsTable <- simulation[[2]]
@@ -121,23 +193,42 @@ test_that("deconvolute rlm", {
     results <- deconvolution[[1]]
     partial_rsq <- deconvolution[[2]]
 
-    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas, by = "IDs"))[, -1])
+    vectorlength <- NROW(tidyr::drop_na(merge(bulkTable, ref_atlas,
+        by = "IDs"
+    ))[, -1])
 
-    expect_output(deconvolute(bulk = bulkTable, model = "rlm"), message("DECONVOLUTION WITH RLM", "\n"))
-    expect_output(deconvolute(bulk = bulkTable, model = "rlm"), message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR RLM : "))
+    expect_output(
+        deconvolute(bulk = bulkTable, model = "rlm"),
+        message("DECONVOLUTION WITH RLM", "\n")
+    )
+    expect_output(
+        deconvolute(bulk = bulkTable, model = "rlm"),
+        message("SUMMARY OF PARTIAL R-SQUARED VALUES FOR RLM : ")
+    )
 
     expect_equal(dim(results), c(5, length(colnames(ref_atlas[, -1]))))
-    expect_equal(dim(deconvolute(bulk = bulkTable[-1, ], model = "rlm")[[1]]), c(5, length(colnames(ref_atlas[, -1]))))
+    expect_equal(
+        dim(deconvolute(bulk = bulkTable[-1, ], model = "rlm")[[1]]),
+        c(5, length(colnames(ref_atlas[, -1])))
+    )
     expect_equal(colnames(results), colnames(ref_atlas[, -1]))
-    expect_equal(lapply(unlist(results[, ]), typeof)[[1]], print("double"), ignore_attr = TRUE)
+    expect_equal(lapply(unlist(results[, ]), typeof)[[1]], print("double"),
+        ignore_attr = TRUE
+    )
     expect_equal(sum(results[1, ]), 1)
     expect_equal(sum(results[2, ]), 1)
     expect_equal(sum(results[3, ]), 1)
     expect_equal(sum(results[4, ]), 1)
     expect_equal(sum(results[5, ]), 1)
     expect_error(deconvolute(bulk = bulkTable[, -1], model = "rlm"))
-    expect_error(deconvolute(bulk = bulkTable, model = "rlm", vec = rep_len(1, vectorlength)), NA)
-    expect_error(deconvolute(bulk = bulkTable, model = "rlm", vec = rep_len(1, vectorlength - 1)))
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "rlm",
+        vec = rep_len(1, vectorlength)
+    ), NA)
+    expect_error(deconvolute(
+        bulk = bulkTable, model = "rlm",
+        vec = rep_len(1, vectorlength - 1)
+    ))
     expect_error(deconvolute(model = "rlm"))
     expect_equal(dim(results), dim(proportionsTable))
     expect_equal(colnames(results), colnames(ref_atlas[, -1]))
