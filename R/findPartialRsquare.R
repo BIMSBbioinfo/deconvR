4de410d785612558049f15a8353c23ab8afd9038
İrem Gündüz <irembgunduz@gmail.com>
2021-10-26 10:28:33 +0300

    Update findPartialRsquare.R

--- R/findPartialRsquare.R
+++ R/findPartialRsquare.R
@@ -3,16 +3,16 @@
 #' @importFrom assertthat assert_that
 #' @importFrom rsq rsq.partial
 #' @importFrom stats lm
-#' @details This function checks if deconvolution brings advantages on top of 
-#' the basic bimodal methylation profiles through partial R-squares. The 
-#' reference matrix usually follows a bimodal distribution in the case of 
-#' methylation, and taking the average of  the rows of methylation matrix might 
+#' @details This function checks if deconvolution brings advantages on top of
+#' the basic bimodal methylation profiles through partial R-squares. The
+#' reference matrix usually follows a bimodal distribution in the case of
+#' methylation, and taking the average of  the rows of methylation matrix might
 #' give a pretty similar profile to the bulk methylation profile you are trying
-#' to deconvolute. If the deconvolution is advantageous, partial R-squared is 
+#' to deconvolute. If the deconvolution is advantageous, partial R-squared is
 #' high.
 #' @param vec The user may provide a vector with which partial R-squared of the
-#' results will be calculated. The length must match the number of rows of the 
-#' reference and bulk tables merged on the ID column (with NAs removed.Defaults 
+#' results will be calculated. The length must match the number of rows of the
+#' reference and bulk tables merged on the ID column (with NAs removed.Defaults
 #' to row means of reference.
 #' @param observed the observed value of the dependent variable
 #' @param predicted the predicted value of the dependent variable
